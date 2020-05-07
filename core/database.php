ff90cdbc3945338a89aae7720d830f30568cf39c
Philip Baumann <philip.baumann@hispeed.ch>
2020-05-07 14:47:35 +0200

    Final

--- core/database.php
+++ core/database.php
@@ -8,19 +8,13 @@
 function connectToDatabase()
 {
     try {
-        if ($_SERVER["SERVER_NAME"] == 'web.kurse.ict-bz.ch') {
-            $pdo = new PDO(
-                'mysql:host=localhost;dbname=kurseictbz_30716',
-                'kurseictbz_30716',
-                'db_307_pw_16'
-            );
-        } else {
-            $pdo = new PDO('mysql:host=127.0.0.1;dbname=credit_company', 'root', '', [
+
+            return new PDO('mysql:host=127.0.0.1;dbname=credit_company', 'root', '', [
                 'mysql:host=localhost;dbname=credit_company',
                 'root',
                 ''
             ]);
-        }
+
     } catch (PDOException $e) {
         die('Keine Verbindung zur Datenbank möglich: ' . $e->getMessage());
     }
