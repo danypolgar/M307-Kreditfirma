40548cea0073364a7c35804afbff4cc25b42de00
Philip Baumann <philip.baumann@hispeed.ch>
2020-05-07 13:12:42 +0200

    Add route

--- routes.php
+++ routes.php
@@ -4,6 +4,7 @@ $router = new Router();
 
 $router->define([
     '' => 'app/Controllers/OverviewController.php',
+    'overview' => 'app/Controllers/OverviewController.php',
     'erfassen' => 'app/Controllers/AddCreditController.php',
     'add' => 'app/Controllers/AddController.php',
     'bearbeiten' => 'app/Controllers/UpdateCreditController.php',
